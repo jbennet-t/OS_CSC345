429c71183dc633162ff6b9f0fc487b75389066f8
osc <sinowayjb@gmail.com>
2021-02-18 16:44:38 -0500

    fixed ex2, still need to finish ex1

--- Lab3/lab03_ex2.c
+++ Lab3/lab03_ex2.c
@@ -9,25 +9,27 @@ int pts_in_circle;
 
 void* run(void* param)
 {
+    int i;
     for(int i = 0; i < total_pts; ++i)
     {
 
         double x = (double)rand() / (double)RAND_MAX *2.0 - 1.0;
         double y = (double)rand() / (double)RAND_MAX *2.0 - 1.0;
-        pthread_exit(0);
+
 
         if (sqrt(x * x + y * y) < 1.0)
         {
             ++pts_in_circle;
         }
     }
+     pthread_exit(0);
 }
 
 
 int main(int argc, char** argv)
 {
     total_pts = atoi(argv[1]);
-    
+
     pthread_t tid;
     pthread_create(&tid, 0, run, NULL);
     pthread_join(tid, NULL);
