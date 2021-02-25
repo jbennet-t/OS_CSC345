89464dabf0dc45e857b0dd82c9bb4a5794428c87
osc <sinowayjb@gmail.com>
2021-02-25 18:43:35 -0500

    working mostly

--- Project1/main.c
+++ Project1/main.c
@@ -7,30 +7,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <wait.h>
 
 #define MAX_LINE 80 /*maximum length command*/
 
 int main(void)
 {
     char *args[MAX_LINE/2+1]; /* command line arguments */
-    char str[MAX_LINE/2+1];
-    char *cmd;
+    char str[MAX_LINE/2+1]; /* temp for cmd line input, not a pointer so it can be stepped through */
+    char *cmd; /* holds arguements from cmd line*/
+    char *token;
+    char *history;
 
     int should_run = 1; /* flag to determine when to exit programs */
-    int argc;
-    int len;
+    int len; /* input command length */
+    int argc; /* arguement counter */
+        int background_process;     /* flag to determine when to run a process in the background -- & */
+    int redirect_out;           /* flag to determine file redirection OUT -- > */
+    int redirect_in;            /* flag to determine file redirection IN  -- < */
+    int pipe_process;           /* flag to determine if the pipe operator is used */ 
+    int pipefd[2];              /* pipe file descriptor */
 
 
-    
     pid_t pid; /*process id*/
     pid_t pid_pipe; /*pipe process id for pipe*/
 
     while(should_run)
     {
-        // background_process = 0;
-        // redirect_out = 0;
-        // redirect_in = 0;
-        // pipe_process = 0;
+        background_process = 0;
+        redirect_out = 0;
+        redirect_in = 0;
+        pipe_process = 0;
         argc = 0;
 
         printf("osh>");
@@ -46,18 +57,164 @@ int main(void)
             str[len-1] = '\0';
         }
 
-        /* allocate mem for command string */
-        cmd = malloc(MAX_LINE/2+1 * sizeof(char));
+        /* preallocate mem for command string */
+        cmd = malloc(sizeof(str) * sizeof(char)); // 41 * 1 byte
+
+        /* check for history command,"!!" */
+        if(strcmp(str, "!!") == 0)
+        {
+            cmd = history;
+            history = malloc(strlen(cmd) * sizeof(char));
+            memcpy(history, cmd, strlen(cmd));
+        }
+        else if(str[0] != '\n' && str[0] != '\0') /*if not history cmd, proceed normally */
+        {
+            cmd = str;
+            history = malloc(MAX_LINE/2+1 * sizeof(char));
+            memcpy(history, str, len);
+        }
+       
+
+        /* tokenize command, breaking up cmd into chunks, delineated by " " */
+        token = strtok(cmd," ");
+
+        while(token != NULL)
+        {
+            /* set each token to a different element of args */
+            args[argc] = token;
+
+            if(strcmp(token,"|") == 0)
+            {
+                pipe_process = argc;
+            }
+
+            token = strtok(NULL, " "); /* clearing token*/
+            argc++;
+        }
+        args[argc] = '\0'; /* set last arguement to NULL so execvp() terminates*/
 
 
+        /* exit case */
+        if(strcmp(str, "exit") == 0)
+        {
+            exit(0);
+        }
 
+        /* directory change */
+        if(strcmp(args[0], "cd") == 0) /*if first token == cd */
+        {
+            chdir(args[1]); /* cd to 2nd token */
+        }
+        
+        /* background process - if last argument is "&"" */
+        if (strcmp(args[argc-1],"&") == 0) 
+        {
+            background_process = 1;
+            args[argc-1] = '\0'; /* clearing last argument */
+        }
 
+        /* redirection handling - if 2nd to last argument is outputting to .txt "> out.txt" */
+        if (argc > 2)
+        {
+            if (strcmp(args[argc-2],">") == 0)
+            {
+                args[argc-2] = '\0';
+                redirect_out = 1;
+            } 
+            else if (strcmp(args[argc-2],"<") == 0) 
+            {
+                args[argc-2] = '\0';
+                redirect_in = 1; 
+            }
+        }
+        
+        /* parent & child forking */
+        pid = fork();
+        
+        if(pid == 0)/* Child process 1 */
+        {
+            if(argc > 0) /* if theres arguments */
+            {
+                if(redirect_out) /* redirecting data out */
+                {
+                    int fdOut;
+                    if((fdOut = creat(args[argc-1], 0644)) < 0)
+                    {
+                        printf("File error - could not create file.\n");
+                        exit(0);
+                    }
+                    dup2(fdOut, STDOUT_FILENO);
+                    close(fdOut);
+                }
+                else if(redirect_in) /* redirecting data in */
+                {
+                    int fdIn;
+                    if((fdIn = open(args[argc-1], O_RDONLY)) < 0 )
+                    {
+                        printf("File error - could not open file.\n");
+                        exit(0);
+                    }
+                    dup2(fdIn, STDIN_FILENO);
+                    close(fdIn);
+                }
 
+                /* 
+                * use | pipe to allow output of one command serve as input for another
+                * have child create another child and use dup2()
+                * assumption: 1 pipe command and no redirect ops at the same time
+                */
 
+                if(pipe_process)
+                {
+                    /* create pipe and handle errors*/
+                    if(pipe(pipefd) < 0)
+                    {
+                        printf("Failed to create the pipe.\n");
+                        exit(1);
+                    }
 
+                    /* fork new process for the child to execute */
+                    pid_pipe = fork();
 
+                    if(pid_pipe == 0) /* Child process #2 */
+                    {
+                        /* redirection for STDOUT to pipe */
+                        dup2(pipefd[1], STDOUT_FILENO);
+                        args[pipe_process] = '\0';
+                        close(pipefd[0]);
+
+                        /* execute first argument */
+                        execvp(args[0],args);
+                    }
+                    else if (pid_pipe > 0) /* parent process #2*/
+                    {
+                        wait(NULL);
+
+                        /* redirection for STDIN to pipe */
+                        dup2(pipefd[0],STDIN_FILENO);
+                        close(pipefd[1]);
+
+                        /* execute args after the pipe "|" */
+                        execvp(args[pipe_process + 1], &args[pipe_process + 1]);
+                    }
+                }
+                else /* invoke execvp() */
+                    execvp(args[0],args); /* executing files */
+            }
+            else
+            {
+                fprintf(stderr,"You have zero arguments.\n");
+            }
+        }
+        else if(pid > 0) // Parent process #1
+        {
+            /* invoke wait unless command included & */
+            if(!background_process)
+            {
+             wait(NULL);
+            }
+        }
         
     }
-
     return 0;
 }
