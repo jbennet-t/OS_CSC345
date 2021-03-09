6ace50de612db062e7b30e1406aef11056da4fae
osc <sinowayjb@gmail.com>
2021-03-09 13:17:32 -0500

    added basic stuff to main.c

--- Project2/main.c
+++ Project2/main.c
@@ -6,23 +6,79 @@
 /* Create struct to pass into each thread as parameters */
 typedef struct
 {
-    int row;
-    int col;
+    int row; /* row index*/
+    int col; /* columm index*/
+
 } parameters;
+
+/*
+void *check_grid(void *params)
+{
+
+}
+*/
+
+/*
+void *check_row(void *params)
+{
+
+}
+*/
+
+/*
+void *check_all_row(void *params)
+{
+
+}
+
+/*
+void *check_col(void *params)
+{
+
+}
+*/
+
+/*
+void *check_all_col(void *params)
+{
+
+}
+*/
+
 
 int main(int argc, char** argv)
 {
     int option = atoi(argv[1]);
-    int num_threads;
-    /*selecting number of threads via user input for option*/
+    int num_threads; /*selecting number of threads via user input for option*/
 
+    int sudoku_table[9][9];
+    int i, j;
+
+    printf("BOARD STATE IN input.txt\n");
+
+    FILE *input;
+    input = fopen('input.txt', "r");
+
+    for(i = 0; i < 9; i++)
+    {
+        for(j = 0; j < 9; j++)
+        {
+            fscanf(input, "%ld", &sudoku_table[i][j]);
+            printf("%d", sudoku_table[i][j]);
+        }
+        printf("\n");
+    }
+    
 
     if(option == 1)
         num_threads == 11; 
-    else if(option == 2)
-        num_threads == 19; /* 9 threads check cols, 9 threads, check rows, 1 master thread */
-    else if(option == 3)
-        num_threads == 
+    else if(option == 2) /* 9 threads check grids, 1 thread checks rows, 9 threads check cols*/
+        num_threads == 19;
+    else if(option == 3) /* 9 threads check grids, 9 threads check rows, 9 threads check cols*/
+        num_threads == 27;
+
+    
 
 
+    return 0;
 }
